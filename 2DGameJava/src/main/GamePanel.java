7ecbeb96201cb0a61ab1c2b8c47d3597d1fed58d
PiozziFederico <piozzifederico@gmail.com>
2023-08-02 00:45:21 +0200

    keyhanlder and fps print

--- 2DGameJava/src/main/GamePanel.java
+++ 2DGameJava/src/main/GamePanel.java
@@ -20,6 +20,11 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    // FPS
+    final int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
+
     /**
      * A thread is a thread of execution in a program. 
      * The Java Virtual Machine allows an application to have multiple threads of execution running concurrently.
@@ -28,11 +33,18 @@ public class GamePanel extends JPanel implements Runnable{
      */
     Thread gameThread;
 
+    // Set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -44,18 +56,52 @@ public class GamePanel extends JPanel implements Runnable{
 
     public void run() { // from Runnable
 
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        long timer = 0;
+        int drawCount = 0;
+
         while(gameThread != null ){ 
             
-            //System.out.println("loop");
+            currentTime = System.nanoTime();
 
-            update();
+            delta += (currentTime - lastTime) / drawInterval;
 
-            repaint(); // call paintComponent method
+            timer += (currentTime - lastTime);
+
+            lastTime = currentTime;
+
+            if(delta >= 1) {
+                update();
+                repaint(); // call paintComponent method
+                delta--;
+                drawCount++;
+            }
+
+            if( timer >= 1000000000){
+                System.out.println("FPS:"+ drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+
         }
 
     }
 
     public void update() {
+
+        if(keyH.upPressed) {
+            playerY -= playerSpeed;
+        } else if(keyH.downPressed) {
+            playerY += playerSpeed;
+        } else if(keyH.leftPressed) {
+            playerX -= playerSpeed;
+        } else if(keyH.rightPressed) {
+            playerX += playerSpeed;
+        }
 
     }
 
@@ -71,7 +117,7 @@ public class GamePanel extends JPanel implements Runnable{
 
         g2.setColor(Color.white);
 
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
 
         
         g2.dispose(); // dispose of this graphics context and release any system resources that it is using (good practice to save some memory)
