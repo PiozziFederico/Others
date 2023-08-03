3801f825feaadcfa690d8e975f7703e4306d0263
PiozziFederico <piozzifederico@gmail.com>
2023-08-03 17:11:01 +0200

    load tiles and tilemanager, implements static map

--- 2DGameJava/src/main/GamePanel.java
+++ 2DGameJava/src/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable{
     
@@ -16,15 +17,16 @@ public class GamePanel extends JPanel implements Runnable{
     final int scale = 3;
 
     public final int tileSize = originalTileSize * scale; // 48x48 tile
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
 
-    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
-    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+    public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
     // FPS
     final int FPS = 60;
 
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
 
     /**
@@ -37,10 +39,6 @@ public class GamePanel extends JPanel implements Runnable{
 
     Player player = new Player(this, keyH);
 
-    // Set player's default position
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
 
     public GamePanel() {
 
@@ -110,6 +108,8 @@ public class GamePanel extends JPanel implements Runnable{
          * coordinate transformations, color management, and text layout
          */
         Graphics2D g2 = (Graphics2D)g;
+
+        tileM.draw(g2);
 
         player.draw(g2);
         
