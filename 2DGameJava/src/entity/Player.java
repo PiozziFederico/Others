a4b0755800718eda46fbd0a5938e8515a56b42ea
PiozziFederico <piozzifederico@gmail.com>
2023-08-04 15:46:17 +0200

    first implementation of item interaction

--- 2DGameJava/src/entity/Player.java
+++ 2DGameJava/src/entity/Player.java
@@ -19,6 +19,10 @@ public class Player extends Entity {
     public final int screenX;
     public final int screenY;
 
+    // how many keys
+    int hasKey = 0;
+
+
     public Player(GamePanel gp, KeyHandler keyH) {
 
         this.gp = gp;
@@ -30,6 +34,8 @@ public class Player extends Entity {
         solidArea = new Rectangle();
         solidArea.x = 8;
         solidArea.y = 16;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
         solidArea.width = 32;
         solidArea.height = 32;
 
@@ -79,10 +85,15 @@ public class Player extends Entity {
                 direction = "right";
             }
 
-            // check tile collision
+            // CHECK TILE COLLISION
             collisionOn = false;
             gp.cChecker.checkTile(this);
 
+            // CHECK OBJECT COLLISION
+            int objIndex = gp.cChecker.checkObject(this, true);
+            pickUpObject(objIndex);
+
+            // IF COLLISION IS FALSE, PLAYER CAN MOVE
             if(!collisionOn) {
 
                 switch(direction) {
@@ -112,6 +123,27 @@ public class Player extends Entity {
             }
         }
 
+    }
+
+    public void pickUpObject(int i) {
+
+        if(i != 999) {
+
+            String objectName = gp.obj[i].name;
+
+            switch(objectName) {
+                case "Key":
+                    hasKey++;
+                    gp.obj[i] = null;
+                    break;
+                case "Door":
+                    if(hasKey > 0) {
+                        gp.obj[i] = null;
+                        hasKey--;
+                    }
+                    break;
+            }
+        }
     }
 
     public void draw(Graphics2D g2) {
--- 2DGameJava/src/object/OBJ_Door.java
+++ 2DGameJava/src/object/OBJ_Door.java
@@ -17,5 +17,7 @@ public class OBJ_Door extends SuperObject {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        collision = true;
     }
 }
